65d96272e4b40f15ca45a6c53691bbc515871f6e
sfn-git <syedfnadeem323@gmail.com>
2022-06-18 12:22:00 -0400

    Close driver on error/end. Image upload feature

--- discord_webhook.py
+++ discord_webhook.py
@@ -1,32 +1,28 @@
 import config
 import requests #dependency
 
-def sendMessage(title, description, doc_url):
-    url = config.data["DISCORD_WEBHOOK_URL"]
+def sendMessage(title, description, doc_url, img_url):
+    urls = config.data["DISCORD_WEBHOOK_URL"]
 
-    #for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook
-    data = {
-        "username" : "FIA Document"
-    }
-
-    data["embeds"] = [
-        {
-            "title": "{}".format(title),
-            "description" : "{}".format(description),
-            "url": "{}".format(doc_url),
-            "image": {
-                    "url": "https://www.fia.com/sites/default/files/styles/panopoly_image_original/public/fia_logo_final_.png",
-                    "height": 160,
-                    "width": 240
-                }
+    for url in urls:
+        #for all params, see https://discordapp.com/developers/docs/resources/webhook#execute-webhook
+        data = {
+            "username" : "FIA Document"
         }
-    ]
 
-    result = requests.post(url, json = data)
+        data["embeds"] = [
+            {
+                "title": "{}".format(title),
+                "description" : "{}".format(description),
+                "url": "{}".format(doc_url),
+                "image": {"url": img_url}
+            }
+        ]
+        result = requests.post(url, json = data)
 
-    try:
-        result.raise_for_status()
-    except Exception as err:
-        print(err)
-    else:
-        print("Payload delivered successfully, code {}.".format(result.status_code))
+        try:
+            result.raise_for_status()
+        except Exception as err:
+            print(err)
+        else:
+            print("Payload delivered successfully, code {}.".format(result.status_code))
--- main.py
+++ main.py
@@ -2,27 +2,61 @@ from discord_webhook import sendMessage
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from sys import exit
+from shutil import rmtree
+from os import mkdir
+import requests
+from pdf2image import convert_from_path
+from upload_img import uploadImg
 
 options = Options()
 options.headless = True
 options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
 options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(options=options, executable_path="./chromedriver")
 
-driver.get("https://www.fia.com/documents/championships/fia-formula-one-world-championship-14/season/season-2022-2005")
-latest_document = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[3]/div[2]/div/div[3]/div/div[2]/div/ul[1]/li/ul/li/ul/li[1]")
-title = latest_document.find_element(By.CLASS_NAME,'title').text
-date = latest_document.find_element(By.CLASS_NAME, "date-display-single").text
-document_url = latest_document.find_element(By.TAG_NAME, "a").get_attribute("href")
-url = document_url
-description = "{}".format(date)
+try:
+    driver = webdriver.Chrome(options=options)
 
-last_date = open('last_date', 'r').read()
-last_title = open('last_title', 'r').read()
+    driver.get("https://www.fia.com/documents/championships/fia-formula-one-world-championship-14/season/season-2022-2005")
+    latest_document = driver.find_element(By.XPATH, "/html/body/div[3]/div/div[3]/div[2]/div/div[3]/div/div[2]/div/ul[1]/li/ul/li/ul/li[1]")
+    title = latest_document.find_element(By.CLASS_NAME,'title').text
+    date = latest_document.find_element(By.CLASS_NAME, "date-display-single").text
+    document_url = latest_document.find_element(By.TAG_NAME, "a")
+    url = document_url.get_attribute("href")
+    description = "{}".format(date)
+    driver.close()
+except Exception as e:
+    print(e)
+    driver.close()
+    exit(1)
+
+try:
+    last_date = open('last_date', 'r').read()
+    last_title = open('last_title', 'r').read()
+except FileNotFoundError as e:
+    last_date = ""
+    last_title = ""
 
 if((last_date == "" or last_title == "") or (last_date != date and last_title != title)):
-    sendMessage(title, description, url)
+    #get pdf
+    try:
+        rmtree('output')
+    except FileNotFoundError:
+        pass
+    r = requests.get(url, allow_redirects=True)
+    open('latest.pdf', 'wb').write(r.content)
+    images = convert_from_path('latest.pdf')
+    mkdir('output')
+    img_path=''
+    for img in images:
+        img_path = './output/page.jpg'
+        img.save(img_path, 'JPEG')
+        break
+
+    img_url = uploadImg(img_path)
+
+    sendMessage(title, description, url, img_url)
     last_date = open('last_date', 'w').write(date)
     last_title = open('last_title', 'w').write(title)
 
